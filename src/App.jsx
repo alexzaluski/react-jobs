4e082beff902d5664e902e07110234cd540d6103
alexzaluski <aleksander.zal@gmail.com>
2024-05-21 11:52:00 +0200

    Moved router into App component

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />)
-      <Route path="*" element={<NotFoundPage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-    </Route>
-  )
-);
-
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />)
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
